2f60868189953c34c07c3985653ecbf37d1150b3
kyle <kylesuen@setsailsoftware.com>
2021-12-19 01:57:32 +0800

    check for max ceiling price

--- src/app.ts
+++ src/app.ts
@@ -44,12 +44,12 @@ const getServerTime = async () => {
 };
 
 const placeOrder = async (
-    price: string,
+    floatPrice: number,
     symbol: string,
     side: "Sell" | "Buy",
     { qty, freeUSDT }: { qty?: number; freeUSDT?: number }
 ) => {
-    const floatPrice = parseFloat(price);
+    // const floatPrice = parseFloat(price);
     const finalPrice =
         side === "Buy"
             ? Math.floor(floatPrice * 1.5 * 100) / 100
@@ -94,6 +94,7 @@ const getOrderBook = async (symbol: string) => {
         const { data } = await axios.get(`${apiUrl}${orderBook}`, {
             params: { ...orderBookparams }
         });
+        console.dir(data.result);
         return data.result;
     } catch (err) {
         console.log(`Error in getting order Book`);
@@ -104,7 +105,7 @@ const getOrderBook = async (symbol: string) => {
 const loopFunction = async (
     symbol: string,
     side: "Buy" | "Sell",
-    options: { qty?: number; freeUSDT?: number }
+    options: { qty?: number; freeUSDT?: number; maxPrice?: number }
 ) => {
     try {
         const orderBook = await getOrderBook(symbol);
@@ -112,17 +113,21 @@ const loopFunction = async (
             if (orderBook.asks.length > 0 || orderBook.bids.length > 0) {
                 console.dir(`Ask Price: ${orderBook.asks[0][0]}`);
                 const price = side === "Buy" ? orderBook.asks[0][0] : orderBook.bids[0][0];
-
-                // const return_code = await placeOrder(price, symbol, side, options);
+                const floatPrice = parseFloat(price);
+                if (options.maxPrice && floatPrice > options.maxPrice) {
+                    console.log(`Ask Price higher than Max Price`);
+                    return { boughtAll: false, latestBalance: undefined };
+                }
+                const return_code = await placeOrder(floatPrice, symbol, side, options);
                 const latestBalance = parseFloat(await getBalance());
                 console.dir(`Free USDT: ${latestBalance}`);
                 if (latestBalance > 20) {
                     return { boughtAll: false, latestBalance };
                 }
-                // if (return_code === -1151) {
-                //     // pair not opened
-                //     return { boughtAll: false, latestBalance };
-                // }
+                if (return_code === -1151) {
+                    // pair not opened
+                    return { boughtAll: false, latestBalance };
+                }
                 return { boughtAll: true, latestBalance };
             }
             console.log(`no ${symbol} order book yet`);
@@ -141,7 +146,7 @@ const loopFunction = async (
     console.dir(`Free USDT: ${freeUSDT}`);
     while (!finish) {
         const [{ boughtAll, latestBalance }] = await Promise.all([
-            loopFunction("REALUSDT", "Buy", { freeUSDT }),
+            loopFunction("IZIUSDT", "Buy", { freeUSDT, maxPrice: 2 }),
             timeout(2200)
         ]);
         finish = boughtAll;
